92e845c8e8ebb8d5bddcee493e481af6967467cf
Steven DiGregorio <sdigregorio@mines.edu>
2020-05-21 10:23:06 -0600

    Initial Commit

---
+++ SyringePumpKit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SyringePumpKit
+//
+//  Created by Steven DiGregorio on 5/21/20.
+//  Copyright © 2020 Steven DiGregorio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
