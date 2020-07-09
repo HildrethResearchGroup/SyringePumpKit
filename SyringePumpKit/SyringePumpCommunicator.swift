7e666b32a52e3fc60b2245dd2eb13ba65b110e89
Steven DiGregorio <sdigregorio@mines.edu>
2020-07-09 14:16:02 -0600

    Added read function

--- SyringePumpKit/SyringePumpCommunicator.swift
+++ SyringePumpKit/SyringePumpCommunicator.swift
@@ -35,11 +35,13 @@ class SyringePumpCommunicator: NSObject {
     
     // Writes commands to the pump
     func write(command: String){
+        
         // waiting a short time between sending commands
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
             let sendCommand = "\(command)\n\r"
             do {
-            try self.serialPort.writeData(sendCommand.data(using: .ascii)!) // someData is an NSData object
+                try self.serialPort.writeData(sendCommand.data(using: .ascii)!) // someData is an NSData object
+                self.read()
             }
             catch{print("failed to send command")}
         }
@@ -50,14 +52,22 @@ class SyringePumpCommunicator: NSObject {
     func serialPort(_ serialPort: SerialPort, didReceive data: Data) {
         let string = String(data: data, encoding: .ascii)
         if string == "\u{02}"{
-//            print("got \(tempStr)")
+            // Start of transmission
             tempStr = ""
         } else if string == "\u{03}" {
-            
+            // End of transmission
            print("Recieved: \(tempStr) from pump")
         } else {
             tempStr += string!
         }
+    }
+    
+    func read(){
+        // waiting a short time before reading commands
+        do{
+        let stringReceived = try serialPort.readString(ofLength: 20)
+            print(stringReceived)
+        } catch {print("Failed to read data from serial port")}
     }
     
     func close(){
